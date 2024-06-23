754f1ed755087c1bc6122ef7d0ad40a123e003c5
Nugiwara_011200 <global-config@example.com>
2024-06-23 14:34:27 +0800

    add sidebar for the inside of the login page

---
+++ components/Sidebar/Sidebar.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { MdDashboardCustomize, MdRestaurantMenu } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      {/* Sidebar Button */}
+      <button
+        onClick={toggleSidebar}
+        aria-controls="separator-sidebar"
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        {isOpen ? (
+          <AiOutlineClose className="text-black" />
+        ) : (
+          <GiHamburgerMenu />
+        )}
+      </button>
+
+      <aside
+        id="separator-sidebar"
+        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform lg:translate-x-0 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } bg-gray-50 dark:bg-gray-800`}
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto">
+          <ul className="space-y-2 font-medium text-center">
+            <a href="/dashboard">
+              <div className="flex items-center justify-center">
+                <Image
+                  src="/logo/foodhub.png"
+                  alt="Logo"
+                  width={100}
+                  height={100}
+                  className="rounded-full"
+                />
+              </div>
+            </a>
+          </ul>
+          <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+            <li>
+              <a
+                href="/dashboard"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <MdDashboardCustomize />
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <FaUserFriends />
+                <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="/menu"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <MdRestaurantMenu />
+                <span className="flex-1 ms-3 whitespace-nowrap">Menu</span>
+              </a>
+            </li>
+
+            <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+                >
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 17 20"
+                  >
+                    <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
+                  </svg>
+                  <span className="ms-3">Upgrade to Pro</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+                >
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 16 20"
+                  >
+                    <path d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z" />
+                  </svg>
+                  <span className="ms-3">Documentation</span>
+                </a>
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </aside>
+
+      {/* Main Content */}
+      <div className="p-4 ml-64">{/* Content here */}</div>
+    </>
+  );
+};
+
+export default Sidebar;
