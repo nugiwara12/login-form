2b0d5a3d6c02b3de95aff2f9c0bec42b3a568c88
Nugiwara_011200 <global-config@example.com>
2024-06-29 22:41:08 +0800

    add title of the foodhub

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import Script from "next/script";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>FoodHub</title>
+      </Head>
+
       {/* Move the script tags inside the Head component */}
       <Script src="https://code.jquery.com/jquery-3.5.1.min.js" />
       <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" />
